11f5a059bd138e943c52d9756bb81a2c6f607d28
Diego Mateo <diemateosandoval@gmail.com>
2023-01-25 19:51:17 -0500

    Add tic tac toe project

---
+++ projects/02-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,23 @@
+import { WINNER_COMBOS } from "../constants";
+
+export const checkWinner = (boardToCheck) => {
+    // revisamos todas los casos posibles de victoria
+    // para ver si ganó el X u O
+    for (const combo of WINNER_COMBOS) {
+        const [a, b, c] = combo;
+        if (
+            boardToCheck[a] &&
+            boardToCheck[a] === boardToCheck[b] &&
+            boardToCheck[a] === boardToCheck[c]
+        ) {
+            return boardToCheck[a];
+        }
+    }
+    //si no hay ganador
+    return null;
+};
+
+export const checkEndGame = (newBoard) => {
+    // revisamos si hay un empate
+    return newBoard.every((square) => square !== null);
+};
